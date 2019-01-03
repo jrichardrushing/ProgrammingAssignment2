62d167682c1cf493ad989ea31431b2975a924943
jrichardrushing <46355552+jrichardrushing@users.noreply.github.com>
2019-01-03 11:38:57 -0700

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,12 @@
+makeMatrix <- function(x = matrix()){
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function()x
+        setInverse <- function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get, setInverse = setInverse, 
+             getInverse = getInverse)
+}
